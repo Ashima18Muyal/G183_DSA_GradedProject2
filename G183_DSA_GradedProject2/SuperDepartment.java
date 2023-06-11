f9934590a5cadcc095704e4f8e4ff7e8c81b0c79
Ashima18Muyal <132137662+Ashima18Muyal@users.noreply.github.com>
2023-06-11 11:11:19 +0530

    Add files via upload

---
+++ G183_DSA_GradedProject2/SuperDepartment.java
@@ -0,0 +1,25 @@
+package com.greatlearning.assign;
+
+	// Super Department class
+	class SuperDepartment {
+	    public String departmentName() {
+	        return "Super Department";
+	    }
+
+	    public String getTodaysWork() {
+	        return "No Work as of now";
+	    }
+
+	    public String getWorkDeadline() {
+	        return "Nil";
+	    }
+
+	    public String isTodayAHoliday() {
+	        return "Today is not a holiday";
+	    }
+	}
+
+	
+
+	
+
